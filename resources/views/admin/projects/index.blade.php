52793b9c6ebd1159fdf7e2a1af9dbb47f3158d82
DavideAtzo <atzo9477@gmail.com>
2023-05-19 16:45:57 +0200

    visualizzazione dati tabelle

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-prova
+    <div class="container">
+        <div class="row">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Project Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <td>{{ $project->id }}</td>
+                            <td>{{ $project->project_title }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
