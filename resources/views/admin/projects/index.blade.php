cfe5d98f4820d2c3bb6f2246862e9067dee58aea
DavideAtzo <atzo9477@gmail.com>
2023-05-19 17:12:25 +0200

    aggiunta visualizzazione creazione data progetto in pagina

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -15,6 +15,7 @@
                         <tr>
                             <td>{{ $project->id }}</td>
                             <td>{{ $project->project_title }}</td>
+                            <td>{{ $project->creation_date }}</td>
                         </tr>
                     @endforeach
                 </tbody>
